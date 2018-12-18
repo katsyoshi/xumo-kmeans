12fb2085d87e0e30eaa9b6133545dbb99116d277
MATSUMOTO, Katsuyoshi <github@katsyoshi.org>
2018-12-18 21:18:27 +0900

    node

--- lib/xumo/kmeans.rb
+++ lib/xumo/kmeans.rb
@@ -2,7 +2,19 @@ require "xumo/kmeans/version"
 
 module Xumo
   module Kmeans
-    class Error < StandardError; end
-    # Your code goes here...
+    class Node
+      def initialize(args)
+        case args
+        when Hash
+          from_hash(args)
+        end
+      end
+
+      def from_hash(**args)
+        args.each do |key, val|
+          define_method("#{key}") { val }
+        end
+      end
+    end
   end
 end
